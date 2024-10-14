d632cd89c5d5069b5182fa62e08158f18ae7ff40
Gabriel B. Di Rezze <bortoletigabriel.drz@gmail.com>
2024-10-14 13:03:13 -0300

    fix: cors added to app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = require('./router');
+const cors = require('cors');
 
 const app = express();
 
-app.use(router);
+app.use(cors({
+    origin: '*'
+}));
+
+app.use(router);    
 
 module.exports = app;
