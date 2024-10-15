b5782453a1a4efbc1197aa9debd3e7a3fe2d4b87
Gabriel B. Di Rezze <bortoletigabriel.drz@gmail.com>
2024-10-15 14:18:56 -0300

    add: students routes

--- backend/src/router.js
+++ backend/src/router.js
@@ -3,6 +3,16 @@ const studentsController = require('./controllers/studentsCtrl');
 
 const router = express.Router();
 
+// students routes
+
 router.get('/students', studentsController.getAll);
+
+router.post('/students/serch', studentsController.getWhere);
+
+router.post('/students', studentsController.create);
+
+router.put('/students/:rm', studentsController.update);
+
+router.delete('/students/:rm', studentsController.deleteByRm)
 
 module.exports = router;
