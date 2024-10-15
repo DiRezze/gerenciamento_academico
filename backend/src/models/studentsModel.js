402a44d625abc18ed2f8e5ec5567afcb8a567bd6
Gabriel B. Di Rezze <bortoletigabriel.drz@gmail.com>
2024-10-15 14:19:38 -0300

    add: students model crud

--- backend/src/models/studentsModel.js
+++ backend/src/models/studentsModel.js
@@ -12,6 +12,66 @@ const getAll = async () => {
     }
 };
 
+const getWhere = async (params) => {
+    try{
+        const connection = await initializeConnection();
+        const [students] = await connection.execute(`SELECT * FROM alunos WHERE ${params}`);
+        return students;
+    } catch (err){
+        console.log("Erro ao executar a query:", err);
+        throw err;
+    }
+}
+
+const create = async (studentData) => {
+    try{
+        const connection = await initializeConnection();
+        const { nome, email, nascimento, telefone} = studentData;
+        const [result] = await connection.execute(
+            'INSERT INTO alunos (nome, email, nascimento, telefone) VALUES (?,?,?,?)',
+            [nome, email, nascimento, telefone]
+        );
+        return result.insertId;
+    } catch (err){
+        console.log("Erro ao criar aluno:", err);
+        throw err;
+    }
+
+}
+
+const update = async (rm, newData) => {
+    try {
+        const connection = await initializeConnection();
+        const { nome, email, nascimento, telefone} = newData;
+        const [result] = await connection.execute(
+            'UPDATE alunos SET nome = ?, email = ?, nascimento = ?, telefone = ? WHERE rm = ?',
+            [nome, email, nascimento, telefone,rm]
+        );
+        return result.affectedRows;
+    } catch (err) {
+        console.log("Erro ao atualizar aluno:", err);
+        throw err;
+    }
+}
+
+const deleteByRm = async (rm) => {
+    try {
+        const connection = await initializeConnection();
+        const [result] = await connection.execute(
+            'DELETE FROM alunos WHERE rm = ?',
+            [rm]
+        );
+        return result.affectedRows;
+    } catch (err) {
+        console.log("Erro ao atualizar aluno:", err);
+        throw err;
+    }
+}
+
 module.exports = {
-    getAll
+    getAll,
+    getWhere,
+    create,
+    update,
+    deleteByRm
 };
