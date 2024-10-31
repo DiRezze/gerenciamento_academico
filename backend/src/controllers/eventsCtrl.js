f94da306865fe84282836289744275460fa8bd77
Gabriel B. Di Rezze <bortoletigabriel.drz@gmail.com>
2024-10-30 22:34:09 -0300

    fix: eventsCtrl catch

--- backend/src/controllers/eventsCtrl.js
+++ backend/src/controllers/eventsCtrl.js
@@ -5,9 +5,9 @@ const getAll = async (req, res) => {
     try{
         const events = await eventsModel.getAll();
         return res.status(200).json(events);
-    }catch (err){
+    } catch (err){
         console.log("Erro ao buscar eventos: ", err);
-        return res.status(500).json({ message: "Erro ao buscar alunos." });
+        return res.status(500).json({ message: "Erro ao buscar eventos." });
     }
 
 };
