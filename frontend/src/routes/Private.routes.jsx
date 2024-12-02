02bcbb546fbc1b6dad7bec1b15e40e1f74344808
Gabriel B. Di Rezze <bortoletigabriel.drz@gmail.com>
2024-12-02 20:43:09 -0300

    fix: Private.routes code style

--- frontend/src/routes/Private.routes.jsx
+++ frontend/src/routes/Private.routes.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
-import { useAuth } from "../contexts/useAuth";  // Ajuste o caminho do seu contexto, se necessário
+import { useAuth } from "../contexts/useAuth";
 
 const PrivateRoute = ({ element }) => {
   const { isAuthenticated } = useAuth();
 
-  return isAuthenticated ? element : <Navigate to="/login" />;
-};
+  return( 
+    isAuthenticated ? element : <Navigate to="/login" />
+  );
+}
 
 export default PrivateRoute;
