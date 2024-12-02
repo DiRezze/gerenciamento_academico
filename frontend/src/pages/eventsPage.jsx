6d6f264f509bf7590866b8157e0e3d943ceed0cc
Gabriel B. Di Rezze <bortoletigabriel.drz@gmail.com>
2024-12-02 20:24:58 -0300

    eventsPage refreshing state

--- frontend/src/pages/eventsPage.jsx
+++ frontend/src/pages/eventsPage.jsx
@@ -1,25 +1,57 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { RefreshCcw } from "lucide-react";
 import EventsNav from '../components/eventsNav';
-import SearchBar from '../components/searchBar';
 import EventBlock from '../components/eventBlock';
 
 const EventsPage = () => {
+
     const [eventList, setEventList] = useState([]);
+    const [isRefreshing, setIsRefreshing] = useState(false);
 
     const fetchEvents = async () => {
         try {
-            const response = await axios.get('http://localhost:8089/events');
-            setEventList(response.data);
+          setIsRefreshing(true);
+          const response = await axios.get('http://localhost:8089/events');
+          setEventList(response.data);
         } catch (err) {
-            console.error('Erro ao buscar eventos:', err);
+          console.error('Erro ao buscar eventos:', err);
+        } finally {
+          setIsRefreshing(false);
         }
+    };
+
+    useEffect(() => {
+        fetchEvents();
+    }, []);
+
+    const handleRefresh = () => {
+        fetchEvents();
     };
 
     return (
         <div className="flex flex-col items-center min-h-[75%] w-full">
             <h1 className="my-4 font-medium">Eventos</h1>
             <EventsNav />
+
+            { isRefreshing ? (
+                <div className="flex flex-row items-center justify-center">
+                    <RefreshCcw className="mx-2  hover:scale-[1.02] refreshing-icon" />
+                    <span>Carregando</span>
+                </div>
+            ):(
+                <div className="flex flex-row items-center justify-center">
+                    <span className="text-gray-200">
+                        {eventList.length > 0
+                            ? `Foram encontrados ${eventList.length} eventos.`
+                            : "Recarregar"}
+                    </span>
+                    <RefreshCcw
+                        className="mx-2 hover:scale-[1.02] refresh-spin-icon"
+                        onClick={handleRefresh}/>
+                </div>
+
+            )}
             <div className="w-full flex flex-col items-center justify-center gap-3">
                 {eventList.length > 0 ? (
                     eventList.map((event) => (
@@ -29,9 +61,6 @@ const EventsPage = () => {
                     <p>Nenhum evento carregado.</p>
                 )}
             </div>
-            <button onClick={fetchEvents} className="mt-4 px-4 py-2 bg-blue-500 text-white rounded">
-                Carregar
-            </button>
         </div>
 
     );
